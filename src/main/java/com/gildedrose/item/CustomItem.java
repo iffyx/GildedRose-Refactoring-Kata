7e59edf3b2c7b4ca1ae2d993d20c19b1a95215e1
Iwona Jaśtak <iwona.jastak1@gmail.com>
2019-11-11 22:26:33 +0100

    before refactoring

--- src/main/java/com/gildedrose/item/CustomItem.java
+++
@@ -1,8 +0,0 @@
-package com.gildedrose.item;
-
-public interface CustomItem {
-    void updateQuality();
-    String getName();
-    int getQuality();
-    int getSellIn();
-}
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,28 +1,24 @@
 package com.gildedrose;
 
-import com.gildedrose.item.CustomItem;
-import com.gildedrose.item.CustomItemFactory;
-
 public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
-        CustomItemFactory factory = new CustomItemFactory();
 
-        CustomItem[] items = new CustomItem[] {
-                factory.createCustomItem("+5 Dexterity Vest", 10, 20), //
-                factory.createCustomItem("Aged Brie", 2, 0), //
-                factory.createCustomItem("Elixir of the Mongoose", 5, 7), //
-                factory.createCustomItem("Sulfuras, Hand of Ragnaros", 0, 80), //
-                factory.createCustomItem("Sulfuras, Hand of Ragnaros", -1, 80),
-                factory.createCustomItem("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                factory.createCustomItem("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                factory.createCustomItem("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                factory.createCustomItem("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
-        int days = 5;
+        int days = 2;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
@@ -30,7 +26,7 @@ public class TexttestFixture {
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
-            for (CustomItem item : items) {
+            for (Item item : items) {
                 System.out.println(item);
             }
             System.out.println();
