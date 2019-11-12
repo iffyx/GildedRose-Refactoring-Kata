4f3790d871af42de654afbf05ee8d84fd4538537
Iwona Jaśtak <iwona.jastak1@gmail.com>
2019-11-12 19:42:13 +0100

    add DefaultItemTest

---
+++ src/test/java/com/gildedrose/DefaultItemTest.java
@@ -0,0 +1,88 @@
+package com.gildedrose;
+
+import com.gildedrose.item.CustomItem;
+import com.gildedrose.item.CustomItemFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DefaultItemTest {
+    private CustomItemFactory factory;
+    private GildedRose app;
+    private CustomItem[] items;
+    private String name = "+5 Dexterity Vest";
+    private int sellIn;
+    private int quality;
+
+    @BeforeEach
+    void setUp(){
+        sellIn = 20;
+        quality = 6;
+        factory = new CustomItemFactory();
+    }
+
+    @Test
+    void createDefaultItemProperlyTest(){
+        items = new CustomItem[] { factory.createCustomItem(name, sellIn, quality) };
+        app = new GildedRose(items);
+
+        assertThat(app.items[0].getName()).isEqualTo(name);
+    }
+
+    @Test
+    void defaultItemShouldDecreaseSellInTest(){
+        items = new CustomItem[] { factory.createCustomItem(name, sellIn, quality) };
+        app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].getSellIn()).isEqualTo(sellIn - 1);
+    }
+
+    @Test
+    void defaultItemShouldDecreaseSellInAfterFewDaysTest(){
+        items = new CustomItem[] { factory.createCustomItem(name, sellIn, quality) };
+        app = new GildedRose(items);
+        int updateCount = sellIn + 2;
+
+        for(int i = 0; i < updateCount; i++)
+            app.updateQuality();
+
+        assertThat(app.items[0].getSellIn()).isEqualTo(sellIn - updateCount);
+    }
+
+    @Test
+    void defaultItemShouldDecreaseSellInUnderZeroTest(){
+        items = new CustomItem[] { factory.createCustomItem(name, sellIn, quality) };
+        app = new GildedRose(items);
+        int updateCount = 3;
+
+        for(int i = 0; i < updateCount; i++)
+            app.updateQuality();
+
+        assertThat(app.items[0].getSellIn()).isEqualTo(sellIn - updateCount);
+    }
+
+    @Test
+    void defaultItemShouldDecreaseQualityTest(){
+        items = new CustomItem[] { factory.createCustomItem(name, sellIn, quality) };
+        app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].getQuality()).isEqualTo(quality - 1);
+    }
+
+    @Test
+    void defaultItemShouldNotDecreaseQualityBelow0Test(){
+        items = new CustomItem[] { factory.createCustomItem(name, sellIn, 1) };
+        app = new GildedRose(items);
+        int updateCount = 3;
+
+        for(int i = 0; i < updateCount; i++)
+            app.updateQuality();
+
+        assertThat(app.items[0].getQuality()).isEqualTo(0);
+    }
+}
